567656333f06cc058e8b226bd82e2781625d82f5
Takako <taka3000806@gmail.com>
2024-07-13 16:43:00 +0900

    add usermodels

--- backend/chickteckapp/views.py
+++ backend/chickteckapp/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User 
+from .models import UserProfile
 
 # Create your views here.
 def homefunc(request):
     return render(request,'home.html',{})
+
+def usersfunc(request):
+    users = User.objects.all()
+    profile = UserProfile.objects.all()
+    # for i in profile:
+    #     print(i.bio)
+    return render(request,'users.html',{'users':users,'profile':profile})
