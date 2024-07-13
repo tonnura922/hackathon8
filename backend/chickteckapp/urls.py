18e32bb8e8791857035fcc5ffb0def8e08fa8a86
Sol-momma <147966936+Sol-momma@users.noreply.github.com>
2024-07-13 18:27:38 +0900

    feature/login

--- backend/chickteckapp/urls.py
+++ backend/chickteckapp/urls.py
@@ -15,9 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
-from .views import homefunc
+from .views import homefunc,loginfunc
 
 
 urlpatterns = [
      path('home/',homefunc,name='home'),
+     path('login/',loginfunc,name='login'),
 ]
--- backend/chickteckapp/views.py
+++ backend/chickteckapp/views.py
@@ -1,5 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth import authenticate, login
 
-# Create your views here.
 def homefunc(request):
     return render(request,'home.html',{})
+
+def loginfunc(request):
+    if request.method == 'post':
+        username = request.POST['userame']
+        email = request.POST['email']
+        password = request.POST['password']
+        if not username or not email or not password:
+            return render(request,'signup.html', {'error': '全てのフィールドを入力してください'})
+        user = authenticate(request,username=username,password=password,email=email)
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            return render(request,'login.html',{'context':'not login'})
+
+    return render(request,'login.html',{'context':'get method'})
